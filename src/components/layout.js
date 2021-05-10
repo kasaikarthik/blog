3d9c4f5b80c66f1b9d93f70b4b866b2dbf8de59c
kasaikarthik <kasaikarthik@gmail.com>
2021-05-11 00:31:10 +0530

    Added social icons

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import { Link, useStaticQuery, graphql } from "gatsby"
 import Navigation from "../components/navigation"
 import 'prismjs/themes/prism-okaidia.css';
 import '@fontsource/poppins'
+import * as Icon from 'react-feather';
 
 
 
@@ -28,7 +29,13 @@ export default ({ children }) => {
       </header>
       {children}
       <footer className="site-footer">
-        <p>&copy; {new Date().getFullYear()} thesaikatrist &bull; Crafted with <span role="img" aria-label="love">💙</span> by <a href="https://w3layouts.com">Sai Karthik</a></p>
+        <p>&copy; {new Date().getFullYear()} thesaikatrist &bull; Crafted with <span role="img" aria-label="love">💙</span> by <a href="https://thesaikatrist.com">Sai Karthik</a></p>
+        <p>
+        <a href="https://twitter.com/thesaikatrist" target="_blank"><Icon.Twitter /></a>&nbsp;&nbsp;&nbsp;
+        <a href="https://linkedin.com/in/kasaikarthik" target="_blank"><Icon.Linkedin /></a>&nbsp;&nbsp;&nbsp;
+        <a href="https://github.com/kasaikarthik" target="_blank"><Icon.GitHub /></a>&nbsp;&nbsp;&nbsp;
+        <a href="mailto:hello@thesaikatrist.com" target="_blank"><Icon.Mail /></a>&nbsp;&nbsp;&nbsp;
+        </p>
       </footer>
     </div>
   )
