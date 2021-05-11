f10a54d8a765a3c25c98a82583e400d7f40dbaaa
kasaikarthik <kasaikarthik@gmail.com>
2021-05-11 18:18:00 +0530

    Changed site name in navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -23,7 +23,7 @@ export default ({ children }) => {
     <div className="site-wrapper">
       <header className="site-header">
         <div className="site-title">
-          <Link to="/">{data.site.siteMetadata.title}</Link>
+          <Link to="/">thesaikatrist</Link>
         </div>
         <Navigation />
       </header>
