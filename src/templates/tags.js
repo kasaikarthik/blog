8d9abf35b51ad362902eeedef377d49e8269a3fe
kasaikarthik <kasaikarthik@gmail.com>
2021-05-15 12:40:35 +0530

    tags template to include thumbnails and date

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -1,31 +1,52 @@
 import React from "react"
 import PropTypes from "prop-types"
 
+
 // Components
 import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
+
 
 const Tags = ({ pageContext, data }) => {
     const { tag } = pageContext
     const { edges, totalCount } = data.allMarkdownRemark
     const tagHeader = `${totalCount} post${
         totalCount === 1 ? "" : "s"
-        } tagged with "${tag}"`
+        } tagged with ${tag}`
 
     return (
         <Layout>
-            <h1>{tagHeader}</h1>
-            <ul>
-                {edges.map(({ node }, i) => {
-                    const { title, path } = node.frontmatter
-                    return (
-                        <li>
-                            <Link key={i} to={path}>{title}</Link>
-                        </li>
-                    )
-                })}
-            </ul>
-            <br />
+            <h1 style={{textAlign: "center", color: "#0d6efd"}}>{tag}</h1>
+            <p style={{fontSize: "x-large" , textAlign: "center"}}>{tagHeader}</p>
+            <ul style={{paddingInlineStart: 0}}>
+            <div className="grids">
+            {edges.map(({ node }, i) => {
+                const { title, path, thumbnail, time, date } = node.frontmatter
+                return (
+                    <article className="card" style={{justifyContent: "center", paddingTop: "15px", paddingBottom: "15px"}}>
+                        <Link to={path}>
+                            {!!thumbnail && (
+                                <img
+                                className="dark-image"
+                                loading="lazy"
+                                src={thumbnail}
+                                alt={title + "- Featured Shot"} />
+                            )}
+                        </Link>
+
+                        <header>
+                            <h2 className="post-title">
+                                <Link key={i} to={path}>{title}</Link>
+                            </h2>
+                            <h3 className="post-meta"> 
+                                {date} - {time} min read<br/>
+                            </h3>
+                        </header>
+                    </article>
+                )
+            })}
+            </div>
+            </ul><br/>
         </Layout>
     )
 }
@@ -66,6 +87,10 @@ export const pageQuery = graphql`
           frontmatter {
             title
             path
+            thumbnail
+            date(formatString: "MMMM DD, YYYY")
+            time
+            author
           }
         }
       }
